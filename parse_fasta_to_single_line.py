03bc9cb97255caee6391d3f94e774c31cf30e4e4
dchen71 <dchen12812@gmail.com>
2016-05-31 12:25:32 -0700

    added fasta parser to single line

---
+++ parse_fasta_to_single_line.py
@@ -0,0 +1,34 @@
+#
+# Builds new text files for fasta files
+#
+
+#Function to read in fasta and flatten it into a single line txt file
+def flatten_fasta(id, organism):
+	#Reads fasta file
+	file_dir = "Data/"
+	fasta_file = file_dir + id + ".fasta"
+	f = open(fasta_file, "r")
+
+	#Array to take in data
+	ref = []
+
+	#Append all the lines from fasta to array
+	for line in f:
+		ref.append(line.rstrip())
+
+	#Close connection
+	f.close()
+
+	#Remove the header
+	ref = ref[1:len(ref)]
+
+	#Join array pieces together to single line
+	ref = "".join(ref)
+
+	#Output single line fasta
+	output_file = "Output/" + organism + "_raw.txt"
+	f = open(output_file, 'w')
+	f.write(ref)
+	f.close()
+
+flatten_fasta("A0A0B4J1F4", "mouse") #Mouse
